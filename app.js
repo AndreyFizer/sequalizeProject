ee32979d271fc04cc7b369184781e177f8a0b962
AndreyFizer <Fizer.AP@outlook.com>
2015-10-15 09:10:59 +0300

    second commit

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
+'use strict';
 var express = require('express');
+var http = require('http');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development';
+}
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('=== >     Server start success in Production version');
+  require('./config/development');
+}
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.RDS_USERNAME,
+    process.env.RDS_PASSWORD,{
+    host     : process.env.RDS_HOSTNAME,
+    dialect  : 'postgres',
+    pool     : {
+        max  : 5,
+        min  : 0,
+        idle : 10000
+    }
+});
+
+var httpServer = http.createServer(app);
+app.set('port', process.env.PORT || 8821);
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/static');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false, limit: 1024 * 1024 * 5}));
+app.use(bodyParser.json({limit: 1024 * 1024 * 5}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -35,6 +62,13 @@ if (app.get('env') === 'development') {
       error: err
     });
   });
-}
+}*/
+
+httpServer.listen(app.get('port'), function () {
+  console.log("=== >     Express server listening on port " + app.get('port'));
+  console.log("=== >     HOST: " + process.env.HOST);
+  console.log("=== >     RDS_HOSTNAME: " + process.env.RDS_HOSTNAME);
+  console.log("=== >     DATABASE: " + process.env.DATABASE);
+});
 
 module.exports = app;
---
+++ config/development.js
@@ -0,0 +1,11 @@
+/**
+ * Created by andrey on 14.10.15.
+ */
+
+process.env.HOST = 'http://localhost:8821';
+process.env.PORT = '8821';
+process.env.RDS_HOSTNAME = 'localhost';
+process.env.RDS_USERNAME = 'postgres';
+process.env.RDS_PASSWORD = 'postgres';
+process.env.RDS_PORT = 5432;
+process.env.DATABASE = 'legal_app_dev';
